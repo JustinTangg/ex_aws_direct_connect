8bee589e2a676ad1f87cfb61eac8fab999ed101b
Justin Tang <justin.tang@vonage.com>
2021-03-26 11:15:15 -0400

    init commit

---
+++ lib/ex_aws_direct_connect.ex
@@ -0,0 +1,18 @@
+defmodule ExAws.DirectConnect do
+  @moduledoc """
+  Documentation for ExAws.DirectConnect.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAws.DirectConnect.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aws_direct_connect_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAws.DirectConnectTest do
+  use ExUnit.Case
+  doctest ExAws.DirectConnect
+
+  test "greets the world" do
+    assert ExAws.DirectConnect.hello() == :world
+  end
+end
